f282a9d76c8566b7ce87cba4f715acaf4917690f
Nguyen Duc Cong <nguyenduccong2005@gmail.com>
2024-10-21 23:23:55 +0700

    Kết nối database và các function cơ bản.
    - đã kết nối được database và tạo ra các function add, delete, edit.

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+    include "models/configDB.php";
+    $db = new Database;
+    $db->connect();
+?>
---
+++ models/configDB.php
@@ -0,0 +1,62 @@
+<?php
+    class Database {
+        private $host = 'localhost';
+        private $username = 'root';
+        private $password = '';
+        private $db_name = 'capy-restaurant';
+
+        private $conn = null;
+        private $res = null;
+
+        public function connect() {
+            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db_name);
+            if (!$this->conn) {
+                echo "failed to connect to database";
+                exit();
+            } else {
+                mysqli_set_charset($this->conn, 'utf8');
+            }
+            return $this->conn;
+        }
+
+        public function execute($sql) {
+            $this->res = $this->conn->query($sql);
+            return $this->res;
+        }
+
+        public function getData() {
+            if($this->res) {
+                $data = mysqli_fetch_array($this->res);
+            } else {
+                $data = 0;
+            }
+            return $data;
+        }
+
+        public function getAllData() {
+            if (!$this->res) {
+                $data = 0;
+            } else {
+                while($datas = $this->getData()) {
+                    $data[] = $datas;
+                }
+            }
+            return $data;
+        }
+
+        public function insertDataAdmin($full_name, $username, $password) {
+            $sql = "INSERT INTO admin(id, full_name, username, password)VALUES(null, '$full_name', '$username', '$password')";
+            return $this->execute($sql);
+        }
+        
+        public function updateDataAdmin($id, $full_name, $username, $password) {
+            $sql = "UPDATE admin SET full_name = '$full_name', username = '$username', password = '$password' WHERE id = '$id'";
+            return $this->execute($sql);
+        }
+
+        public function deleteAdmin($id) {
+            $sql = "DELETE FROM admin WHERE id = '$id'";
+            return $this->execute($sql);
+        }
+    }
+?>
